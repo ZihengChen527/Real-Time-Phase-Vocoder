5af8bd8f402e27e72a160c1fb7da0f9fbe3b67c2
Ziheng Chen <zihengchen@Zihengs-MacBook-Pro.local>
2014-11-04 10:17:48 +0800

    Initial Commit

---
+++ phasevocoder/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  phasevocoder
+//
+//  Created by Ziheng Chen on 11/4/14.
+//  Copyright (c) 2014 cmu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
